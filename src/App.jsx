0e6bdd8a970a64622510be9ae1dc7f78819fa2ca
Arvin Jimdar Mahato <135803094+arvinpro@users.noreply.github.com>
2025-06-26 23:38:13 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ function App() {
   return (
     <div className=' bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] overflow-hidden'>
     <Hero />
+  <div>
     <About />
+  </div>  
     <Skills/>
     <Projects />
     <Contact />
